8a439a023ae90be327e6f359413c6a453029ce45
Michael Craig <michaelcraigcoding@gmail.com>
2024-04-09 12:13:50 -0400

    Scrape Function done; Work on Async/Recursive Functionality

--- Scrapers/BCPAOScraperV2.py
+++ Scrapers/BCPAOScraperV2.py
@@ -14,11 +14,11 @@ Notes:
 from utils import CHROMIUM_PATH
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import pandas as pd, asyncio, random, csv
+import pandas as pd, asyncio, random, csv, re
 
-def FetchBrowser(driver, URL): # Fetches website; Returns page source
+async def FetchBrowser(driver, URL): # Fetches website; Returns page source
     driver.get(URL)
-    #await asyncio.sleep(1)
+    await asyncio.sleep(1)
     r = driver.page_source
     return r
 
@@ -31,33 +31,35 @@ async def PopulateURLs(property_accounts): # Creates Batch of URLs to pass to he
 
     return URL_List    
 
-def Scrape(response): # Scrapes the data from URL
+async def Scrape(response, WINDOWS_DIR_ONE, WINDOWS_DIR_TWO): # Scrapes the data from URL
     soup = BeautifulSoup(response, 'html.parser')
 
     first_table = soup.find('div', class_="cssDetails_TopContainer cssTableContainer cssOverFlow_x cssCanReceiveFocus", role='region') # Account Data
     second_table = soup.find('table', class_='data-table cssWidth100') # Value Data
-    third_table = soup.find('table', class_='tSalesTransfers') # Sales and Transfers Data
-    # There is a fourth table for building data, but I don't think it's very applicable to what we're predicting
+    # Transfer and Building Tables aren't applicable to the data collection
 
-    if first_table is not None: # If these tables exist, collect the data from them
+    # Initialize variables outside of clauses
+    OWNER = ''
+    MAIL_ADDRESS = ''
+    SITE_ADDRESS = ''
+    PARCEL_ID = ''
+    TAXING_DISTRICT = ''
+    EXEMPTIONS = ''
+    PROPERTY_USE = ''
+    ACERAGE = ''
+    SITE_CODE = ''
+    PLAT_BOOK = ''
+    SUBDIVISION = ''
+    DESCRIPTION = ''
+        
+    MARKETVAL_2021 = ''
+    MARKETVAL_2022 = ''
+    MARKETVAL_2023 = ''
+
+    # If these tables exist, collect the data from them
+    if first_table is not None: 
         layer_one = soup.find('div',class_='cssDetails_Top_Details')
         rows = layer_one.find_all('div', class_='cssDetails_Top_Row')
-        for row in rows:
-            print(row.prettify())
-            print('----------------------------------------------------------------')
-        # Initialize variables outside the loop
-        OWNER = ""
-        MAIL_ADDRESS = ""
-        SITE_ADDRESS = ""
-        PARCEL_ID = ""
-        TAXING_DISTRICT = ""
-        EXEMPTIONS = ""
-        PROPERTY_USE = ""
-        ACERAGE = ""
-        SITE_CODE = ""
-        PLAT_BOOK = ""
-        SUBDIVISION = ""
-        DESCRIPTION = ""
 
         for row in rows:
                 layer_one = row.find('div',class_='cssDetails_Top_Cell_Data')
@@ -92,8 +94,8 @@ def Scrape(response): # Scrapes the data from URL
                     PROPERTY_USE = row.find('div',class_='cssDetails_Top_Cell_Data', attrs={'data-bind':'text: publicUseCode'}).text.strip()
                     continue
 
-                if row.find('div',class_='cssDetails_Top_Cell_Data', attrs={'data-bind':'text: acerage'}):
-                    ACERAGE = row.find('div',class_='cssDetails_Top_Cell_Data', attrs={'data-bind':'text: acerage'}).text.strip()
+                if row.find('div',class_='cssDetails_Top_Cell_Data', attrs={'data-bind':'text: acreage'}):
+                    ACERAGE = row.find('div',class_='cssDetails_Top_Cell_Data', attrs={'data-bind':'text: acreage'}).text.strip()
                     continue                
 
                 if row.find('div',class_='cssDetails_Top_Cell_Data', attrs={'data-bind':'text: siteCodeDesc'}):
@@ -109,40 +111,68 @@ def Scrape(response): # Scrapes the data from URL
                     continue
 
                 if row.find('div',class_='cssDetails_Top_Cell_Data', attrs={'data-bind':'text: legalDescription'}):
-                    SUBDIVISION = row.find('div',class_='cssDetails_Top_Cell_Data', attrs={'data-bind':'text: legalDescription'}).text.strip()
+                    DESCRIPTION = row.find('div',class_='cssDetails_Top_Cell_Data', attrs={'data-bind':'text: legalDescription'}).text.strip()
                     continue                
 
-        print("OWNER:", OWNER)
-        print("SITE_ADDRESS:", SITE_ADDRESS)
-        print("MAIL_ADDRESS:", MAIL_ADDRESS)
-        print("PARCEL_ID:", PARCEL_ID)
-        print("TAXING_DISTRICT:", TAXING_DISTRICT)
-        print("EXEMPTIONS:", EXEMPTIONS)
-        print("PROPERTY_USE:", PROPERTY_USE)
-        print("ACERAGE:", ACERAGE)
-        print("SITE_CODE:", SITE_CODE)
-        print("PLAT_BOOK:", PLAT_BOOK)
-        print("SUBDIVISION:", SUBDIVISION)
-        print("DESCRIPTION:", DESCRIPTION)
+        if second_table is not None: # Needs to be in first_table clause because we won't know what the history is w/o the name
+            MARKETVAL_2021 = second_table.find('td', attrs={'data-bind': 'text: marketVal'}).text.strip()
+            MARKETVAL_2022 = second_table.find('td', attrs={'data-bind': 'text: marketVal1'}).text.strip()
+            MARKETVAL_2023 = second_table.find('td', attrs={'data-bind': 'text: marketVal2'}).text.strip()
 
-        #if second_table is not None:
-        
-        #if third_table is not None:
+            print(OWNER)
+            print(MAIL_ADDRESS)
+            print(SITE_ADDRESS)
+            print(PARCEL_ID)
+            print(TAXING_DISTRICT)
+            print(EXEMPTIONS)
+            print(PROPERTY_USE)
+            print(ACERAGE)
+            print(SITE_CODE)
+            print(PLAT_BOOK)
+            print(SUBDIVISION)
+            print(DESCRIPTION)     
+            print(MARKETVAL_2021)
+            print(MARKETVAL_2022)
+            print(MARKETVAL_2023)
+             
 
-    # Place functionality for first_table not existing here;
-            
+        # Write the values to .csv here
+        with open(WINDOWS_DIR_ONE, 'a', newline='') as file_one:
+            writer_one = csv.writer(file_one)
+            if file_one.tell() == 0:
+                writer_one.writerow(['Owner(s)', 'Mail Address', 'Site Address', 'Parcel ID', 'Taxing District','2023 Exemptions', 'Property Use', 'Acerage', 'Site Code','Plat Book/Page', 'Subdivision', 'Land Description', '2023 Market Value', '2022 Market Value', '2021 Market Value'])
+            writer_one.writerow([OWNER,MAIL_ADDRESS,SITE_ADDRESS,PARCEL_ID,TAXING_DISTRICT,EXEMPTIONS,PROPERTY_USE,ACERAGE,SITE_CODE,PLAT_BOOK,SUBDIVISION,DESCRIPTION,MARKETVAL_2021,MARKETVAL_2022,MARKETVAL_2023])
+        return
+    
+    # If table_one is missing, write the ID to the missing csv to comb through later
+    if first_table is None:
+        missing_layer_one = soup.find('div',id='divSearchDetails_Info', class_='cssSearchDetails_Panel cssPanelBorder_Top')
+        missing_data = missing_layer_one.find('span', class_='spnDetailsRenum').text
+        number = re.search(r'\d+', missing_data).group() # Pulls just the number from the string
+
+        with open(WINDOWS_DIR_TWO, 'a', newline='') as file_two:
+            writer_two = csv.writer(file_two)
+            if file_two.tell() == 0:
+                writer_two.writerow(['ID'])
+            writer_two.writerow([number])    
+
+        return
+    
+    return
+ 
+async def UpdateIDs(property_accounts):
+    if property_accounts:  # Check if the list is not empty
+        last_id = property_accounts[-1]  # Get the last ID
+        property_accounts[0] = last_id + 1  # Set the first element to last_id + 1
+        for i in range(1, len(property_accounts)):  # Update the rest of the list
+            property_accounts[i] = property_accounts[0] + i
+    return property_accounts  
+
 async def main():
     property_accounts = [2000001, 2000002, 2000003, 2000004, 2000005, 2000006, 2000007, 2000008, 2000009, 2000010]
     WINDOWS_DIR_ONE = 'Data Storage\\BCPAOData\\BCPAO_Data.csv'
     WINDOWS_DIR_TWO = 'Data Storage\\BCPAOData\\BCPAO_Data_Missing.csv'
     MAC_DIR = '' # Fill in when writing in MAC environment
-
-    '''
-    with open(WINDOWS_DIR_TWO, 'a', newline='') as file_two: # Place this code in Scrape() function later
-        writer_two = csv.writer(file_two)
-        if file_two.tell() == 0:
-            writer_one.writerow(['Owner(s)', 'Mail Address', 'Site Address', 'Parcel ID', 'Taxing District','2023 Exemptions', 'Property Use', 'Acerage', 'Site Code','Plat Book/Page', 'Subdivision', 'Land Description', '2023 Market Value', '2022 Market Value', '2021 Market Value'])
-    '''
 
     property_URLs = await PopulateURLs(property_accounts)
 
@@ -152,7 +182,10 @@ async def main():
     
     for URL in property_URLs:
         response = await FetchBrowser(driver, URL)
-        await Scrape(response)
+        await Scrape(response, WINDOWS_DIR_ONE, WINDOWS_DIR_TWO)
+
+    new_accounts = await UpdateIDs(property_accounts)
+
 
 async def main_wrapper():
     await main()
@@ -160,7 +193,8 @@ async def main_wrapper():
 #asyncio.run(main_wrapper())
 
 '''----Test Environment Below----'''
-
+# Remove async from scrape and FetchBrowser to test
+'''
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 driver = webdriver.Chrome(options=options)
@@ -171,3 +205,4 @@ WINDOWS_DIR_TWO = 'Data Storage\\BCPAOData\\BCPAO_Data_Missing.csv'
   
 response = FetchBrowser(driver, URL)
 Scrape(response)
+'''
